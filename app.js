ac945c9dd939f58792b396f53ac3ae6cd3b6eb6b
TakahiroYamamoto <bukubuku.awadatu@gmail.com>
2018-11-19 23:14:43 +0900

    git 練習２

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var bodyPaser = require('body-parser');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var anime = require('./routes/anime');
