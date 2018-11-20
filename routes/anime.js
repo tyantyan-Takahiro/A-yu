3500279d560528ccdce8552c3c530adfad14e9bd
TakahiroYamamoto <bukubuku.awadatu@gmail.com>
2018-11-20 11:02:15 +0900

    2018/11/20

--- routes/anime.js
+++ routes/anime.js
@@ -48,18 +48,19 @@ router.get('/detail',(req, res, next) => {
 //POST処理
 router.post('/detail',(req, res, next) => {
   console.log("アニメ詳細ページPOST");
-  var id = req.body.id;//ANIMNE_ID
+  var id = req.body;//ANIMNE_ID
   console.log(id);
   
   //コネクションの用意
   var connection = mysql.createConnection(mysql_setting);
 
-  //データベースに接続
+  //DBに接続
   connection.connect();
   
   //データを取り出す
-  connection.query('SELECT * from anime_table' ,function(error,results, fields){
+  connection.query('SELECT * from `anime_table` WHERE `ANIME_ID` = ' + id,function(error,results, fields){
        //データベースアクセス完了時の処理
+       console.log(results);
           
           if(error == null){
               var data = {
